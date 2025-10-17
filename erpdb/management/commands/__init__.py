10d4194bfcbdbca9a0cbdf0ce54d1082f0fe1b5f
Vince <vpobejas@student.hau.edu.ph>
2025-10-17 09:40:05 +0800

    Fix management commands __init__.py file

--- erpdb/management/commands/__init__.py
+++ erpdb/management/commands/__init__.py
@@ -1,17 +1,1 @@
 # Commands
-from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
-
-class Command(BaseCommand):
-    help = 'Make user a superuser'
-
-    def handle(self, *args, **options):
-        try:
-            user = User.objects.get(username='yuzuruw')
-            user.is_staff = True
-            user.is_superuser = True
-            user.save()
-            self.stdout.write(self.style.SUCCESS(f'Successfully made {user.username} a superuser!'))
-        except User.DoesNotExist:
-            self.stdout.write(self.style.ERROR('User yuzuruw not found'))
-
